7cf4a28813c83fd72c9a6d5fad6a2abfcbd09279
David Lawrie <dlawrie@att.net>
2019-05-17 16:59:43 -0500

    Completed text for home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import { Link } from "react-router-dom";
 import Navbar from '../components/jsx/navbar.jsx';
 import Jumbotron from '../components/jsx/jumbotron.jsx';
 import Footer from '../components/jsx/footer.jsx';
+import '../pages/css/home.css';
 
 class Home extends Component {
     render() {
@@ -10,8 +12,16 @@ class Home extends Component {
                 <Navbar />
                 <Jumbotron title='Be Prepared' subtitle='When disaster strikes.'/>
               <div className='container'>
-                 <p>Lorem ipsum dolor sit amet, mea no cotidieque ullamcorper, no per pericula torquatos deterruisset. Pro corpora singulis consetetur et, consul graeco assentior ad pri. Mea te saepe dissentiet, nec ex cetero conclusionemque. No mel evertitur vituperata, ea ornatus accumsan euripidis mei. Postea efficiantur sed no, nam homero intellegat adipiscing ne, hinc illum per eu.
-                 </p>
+                 <h4 className='main-text'>According to the National Oceanic and Atmospheric Administartion (NOAA),<br />
+                  natural disasters caused over $90 billion in losses in the US in 2018 alone.*<br />
+                 With an up to date home inventory you can ensure that you have the insurance coverage you need when the unthinkable happens.
+                 <p></p>
+                 Our web based solution will allow you to save information regarding your personal property on
+                 a secure, remote database ready for you to access at any time. Don't rely on your memory
+                 when it's time to make a claim, get everything you're entitled to with our Home Inventory
+                 application. Click here to<Link to='/signup'> sign up</Link> for your account or use the link on the top right of this page. 
+                 </h4>
+                 <p className='citation'>* <Link to='https://www.ncdc.noaa.gov/billions/'>https://www.ncdc.noaa.gov/billions/</Link></p>
               </div>
               <Footer />
             </div>
